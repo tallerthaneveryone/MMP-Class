ffdfe5fe0d54736742d6febd9ba255e10a57ed70
tallerthaneveryone <70382588+tallerthaneveryone@users.noreply.github.com>
2020-09-24 18:02:02 -0400

    Self portrait one update

---
+++ self_portrait_v1/sketch.js
@@ -0,0 +1,39 @@
+/* 
+Self Portrait Version 1
+by Brendan Doherty
+*/
+
+var faceSize = "200" * 3;
+
+function setup() {
+    createCanvas(600,360);
+}
+
+function   draw() {
+background('green');
+
+
+
+// face
+fill(225);
+
+
+// eyes
+fill(360)
+circle(180, 90, 80) // left
+circle(300,90,80) // right 
+
+// nose
+fill(255)
+square(195,150, 50)
+
+// mouth
+fill(255)
+square(190,210, 120)
+
+// hair I'm bald
+
+
+
+
+}
